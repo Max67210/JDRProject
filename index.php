9e7d0cc36b60962b415ff15125e6b2248106ce35
maxim <maxim@DESKTOP-VLH5K36>
2017-12-09 18:22:50 +0100

    Mise en place

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            include 'Controllers/HomePageController.php';
+        ?>
+    </body>
+</html>
 
-phpinfo();
 
-?>
 
